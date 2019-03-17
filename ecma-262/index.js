e8e7ad4cee22f8e64dd397a173668d5715ccad89
Evgeniy Ostapenko <ostapenko.zheka@gmail.com>
2019-03-17 13:28:41 +0300

    добавил перемешку

--- ecma-262/index.js
+++ ecma-262/index.js
@@ -1,10 +1,10 @@
 const readline = require('readline');
 const SLL = require('./singly-linked-list');
-
+let y = {};
 let list = new SLL(1,2,3);
 
 list.addToHead(-1,0,0.9);
 
 list.addToTail(10, {v: 5}, 25);
 
-console.log('' + list.toArray());
+
