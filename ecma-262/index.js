cd058f9a99c7f2d47be491083cbf24fe18a19a29
Evgeniy Ostapenko <ostapenko.zheka@gmail.com>
2019-03-17 13:24:29 +0300

    home-1111 добавил переменную

--- ecma-262/index.js
+++ ecma-262/index.js
@@ -1,5 +1,6 @@
 const readline = require('readline');
 const SLL = require('./singly-linked-list');
+const a = 10;
 
 let list = new SLL(1,2,3);
 
@@ -7,4 +8,4 @@ list.addToHead(-1,0,0.9);
 
 list.addToTail(10, {v: 5}, 25);
 
-console.log('' + list.toArray());
+
